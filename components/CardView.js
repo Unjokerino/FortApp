8092075f232953be007e0765a5dee5beb8d3e29c
Unjokerino <35525106+Unjokerino@users.noreply.github.com>
2019-12-05 16:58:02 +0500

    initial commit

---
+++ components/CardView.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export function CardView(props) {
+  return (
+    <View style={styles.card}>
+      <Text>{props.name}asdad</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: "column",
+    backgroundColor: "white",
+    width: 150,
+    height: 150
+  }
+});
